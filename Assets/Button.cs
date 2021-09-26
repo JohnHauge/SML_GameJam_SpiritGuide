78e8b7d4bc8d40b8cb737f818d379ce7e7387f70
Vegar Kristoffer Kvammen <Vegark16@uia.no>
2021-09-26 15:01:58 +0200

    q

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Button : MonoBehaviour
 {
@@ -9,11 +10,13 @@ public class Button : MonoBehaviour
     private Ghost ghost;
     private Collider2D trigger;
     private SpriteRenderer renderer;  
+    private UnityEvent startEvent;
     // Start is called before the first frame update
     void Start()
     {
         trigger = gameObject.GetComponent(typeof(Collider2D)) as Collider2D;
         renderer = gameObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+        startEvent = new UnityEvent();
     }
 
     // Update is called once per frame
@@ -27,6 +30,7 @@ public class Button : MonoBehaviour
             //renderer.color = new Color(254, 116, 54);
             renderer.color = Color.green;
             ghost.powerup = 0;
+            startEvent.Invoke();
         }
     }
 }
