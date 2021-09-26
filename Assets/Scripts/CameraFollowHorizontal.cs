6e3ab17f4575bbed778bfbdd7bf44293f95a385e
JohnH <john-gustav@hotmail.com>
2021-09-26 16:17:14 +0200

    Tutorial2

---
+++ Assets/Scripts/CameraFollowHorizontal.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollowHorizontal : MonoBehaviour {
+
+    private Camera _camera;
+    [SerializeField] private Transform bodyTransform;
+    [SerializeField] private float BodyMaxLeft;
+    [SerializeField] private float BodyMaxRight;
+
+    [SerializeField] private float CameraMaxLeft;
+    [SerializeField] private float CameraMaxRight;
+
+    private void Awake() {
+        _camera = Camera.main;
+    }
+
+    private void Update() {
+        float cameraLerp = Mathf.InverseLerp(BodyMaxLeft, BodyMaxRight,bodyTransform.position.y);
+
+        _camera.transform.position = new Vector3(Mathf.Lerp(CameraMaxRight,CameraMaxLeft, cameraLerp), 0f, -10f);
+    }
+}
