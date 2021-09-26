4875f3c42cf807606cb41b9d9752db28547835e9
JohnH <john-gustav@hotmail.com>
2021-09-26 15:29:34 +0200

    Diverse

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField] int LevelToLoad;
+
+    private bool isLoading = false;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            loadNewLevel();
+        }
+    }
+
+    public void loadNewLevel(){
+        if(isLoading) return;
+        
+        isLoading = true;
+
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
