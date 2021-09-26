0a76165b888b246d2cafa88a572337ea9f547516
JohnH <john-gustav@hotmail.com>
2021-09-26 13:53:53 +0200

    Level01 & cameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,5 +1,22 @@
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
-    
+
+    private Camera _camera;
+    [SerializeField] private Transform bodyTransform;
+    [SerializeField] private float maxBodyHeight;
+    [SerializeField] private float minBodyHeight;
+
+    [SerializeField] private float maxCameraHeight;
+    [SerializeField] private float MinCameraHeight;
+
+    private void Awake() {
+        _camera = Camera.main;
+    }
+
+    private void Update() {
+        float cameraLerp = Mathf.InverseLerp(minBodyHeight,maxBodyHeight,bodyTransform.position.y);
+
+        _camera.transform.position = new Vector3(0f, Mathf.Lerp(MinCameraHeight,maxCameraHeight, cameraLerp), -10f);
+    }
 }
